7ec6df1c5973b735166c1a8195ef77d374539d81
Markoole <marko@banaanisaaret.com>
2016-09-26 13:22:11 +0100

    Exercise 5, refactoring 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 export const ContactsAppRoutes = [
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
